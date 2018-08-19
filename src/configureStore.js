24e028a6a627fb13d318ba09b590ce8459885fe3
IsmaelCovarrubias <ismael_covarrubias@intuit.com>
2018-08-19 14:27:28 -0700

    18. Applying Redux Middleware

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,49 +1,22 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import promise from 'redux-promise';
+import {createLogger} from 'redux-logger';
 import todoApp from './reducers';
 
-const logger = (store) => (next) => {
-	if (!console.group) {
-		return next;
-	}
 
-	return (action) => {
-		console.group(action.type);
-		console.log('%c prev state', 'color: gray', store.getState());
-		console.log('%c action', 'color: blue', action);
-		const returnValue = next(action);
-		console.log('%c nextState', 'color: green', store.getState());
-		console.groupEnd(action.type);
-		return returnValue;
-	};
-};
-
-const promise = (store) => (next) => (action) => {
-	if (typeof action.then === 'function') {
-		return action.then(next);
-	}
-	return next(action);
-};
-
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-	middlewares.slice().reverse().forEach(middleware => 
-		store.dispatch = middleware(store)(store.dispatch)
-	);
-};
 
 const configureStore = () => {
-	const store = createStore(
+	const middlewares = [promise];
+	if(process.env.NODE_ENV !== 'production') {
+		middlewares.push(createLogger());
+	}
+
+	//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	return createStore(
 		todoApp,
 		// persistedState,
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		applyMiddleware(...middlewares)
 	);
-
-	const middlewares = [promise];
-
-	middlewares.push(logger);
-
-	wrapDispatchWithMiddlewares(store, middlewares);
-
-	return store;
 }
 
 export default configureStore;
