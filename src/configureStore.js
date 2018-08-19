625241b8a400c1c8c13487a136e0d25dfda13132
IsmaelCovarrubias <ismael_covarrubias@intuit.com>
2018-08-19 14:20:57 -0700

    17. The middleware chain

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,31 +1,33 @@
 import {createStore} from 'redux';
 import todoApp from './reducers';
 
-const addLoggingSupportToDispatch = (store) => {
-	const rawDispatch = store.dispatch;
+const logger = (store) => (next) => {
 	if (!console.group) {
-		return rawDispatch;
+		return next;
 	}
 
 	return (action) => {
 		console.group(action.type);
 		console.log('%c prev state', 'color: gray', store.getState());
 		console.log('%c action', 'color: blue', action);
-		const returnValue = rawDispatch(action);
+		const returnValue = next(action);
 		console.log('%c nextState', 'color: green', store.getState());
 		console.groupEnd(action.type);
 		return returnValue;
 	};
 };
 
-const addPromiseSupportToDispatch = (store) => {
-	const next = store.dispatch;
-	return (action) => {
-		if (typeof action.then === 'function') {
-			return action.then(next);
-		}
-		return next(action);
+const promise = (store) => (next) => (action) => {
+	if (typeof action.then === 'function') {
+		return action.then(next);
 	}
+	return next(action);
+};
+
+const wrapDispatchWithMiddlewares = (store, middlewares) => {
+	middlewares.slice().reverse().forEach(middleware => 
+		store.dispatch = middleware(store)(store.dispatch)
+	);
 };
 
 const configureStore = () => {
@@ -35,8 +37,11 @@ const configureStore = () => {
 		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 	);
 
-	store.dispatch = addLoggingSupportToDispatch(store);
-	store.dispatch = addPromiseSupportToDispatch(store);
+	const middlewares = [promise];
+
+	middlewares.push(logger);
+
+	wrapDispatchWithMiddlewares(store, middlewares);
 
 	return store;
 }
