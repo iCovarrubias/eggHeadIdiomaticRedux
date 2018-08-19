8f22088857f4917bb0dc8a5282b3b35681d4f973
IsmaelCovarrubias <ismael_covarrubias@intuit.com>
2018-08-19 13:58:21 -0700

    16. Wrapping dispatch() to recognize promises

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -3,7 +3,6 @@ import {connect} from 'react-redux';
 import {withRouter} from 'react-router';
 import * as actions from '../actions';
 import { getVisibleTodos } from '../reducers'
-import { fetchTodos } from '../api';
 
 
 //presentational component
@@ -49,10 +48,8 @@ class VisibleTodoList extends React.Component {
 	}
 
 	fetchData() {
-		const {filter, receiveTodos} = this.props;
-		fetchTodos(filter).then(todos => 
-			receiveTodos(filter, todos)
-		)
+		const {filter, fetchTodos} = this.props;
+		fetchTodos(filter);
 	}
 
 	render() {
--- src/configureStore.js
+++ src/configureStore.js
@@ -1,12 +1,25 @@
 import {createStore} from 'redux';
 import todoApp from './reducers';
 
+
+const addPromiseSupportToDispatch = (store) => {
+	const rawDispatch = store.dispatch;
+	return (action) => {
+		if (typeof action.then === 'function') {
+			return action.then(rawDispatch);
+		}
+		return rawDispatch(action);
+	}
+}
+
 const configureStore = () => {
 	const store = createStore(
 		todoApp,
 		// persistedState,
 		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 	);
+
+	store.dispatch = addPromiseSupportToDispatch(store);
 
 	return store;
 }
