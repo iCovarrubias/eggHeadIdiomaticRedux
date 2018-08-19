0114456d972347b7fe49fa3a448247c392fc392f
IsmaelCovarrubias <ismael_covarrubias@intuit.com>
2018-08-19 11:57:03 -0700

    13. Remove localStorage related code

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,21 +1,12 @@
 import {createStore} from 'redux';
-import {throttle} from 'lodash';
 import todoApp from './reducers/reducers';
-import {loadState, saveState} from './localStorage';
 
 const configureStore = () => {
-	const persistedState = loadState();
 	const store = createStore(
 		todoApp,
 		// persistedState,
 		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 	);
-
-	store.subscribe(throttle(() => {
-		saveState({
-			todos: store.getState().todos
-		});
-	}, 1000));
 
 	return store;
 }
