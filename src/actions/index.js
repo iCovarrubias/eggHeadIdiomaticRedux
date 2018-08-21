39b337fb5e99619cde3421f8837f27bec984c288
IsmaelCovarrubias <ismael_covarrubias@intuit.com>
2018-08-20 17:58:08 -0700

    26. Normalizing responses with normalizr

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,8 @@
 import * as api from '../api';
-import {getIsFetching} from '../reducers';
+import { normalize } from 'normalizr';
+import * as schema from './schema';
+import { getIsFetching } from '../reducers';
+
 
 export const fetchTodos = (filter) => (dispatch, getState) => {
 	if (getIsFetching(getState(), filter)) {
@@ -15,7 +18,7 @@ export const fetchTodos = (filter) => (dispatch, getState) => {
 		dispatch({
 			type: 'FETCH_TODOS_SUCCESS',
 			filter,
-			response
+			response: normalize(response, schema.arrayOfTodos)
 		}),
 		error => {
 			dispatch({
@@ -31,8 +34,8 @@ export const fetchTodos = (filter) => (dispatch, getState) => {
 export const addTodo = (text) => (dispatch) =>
 	api.addTodo(text).then(response => {
 		dispatch({
-			type: 'ADD_TODO_SUCESS',
-			response
+			type: 'ADD_TODO_SUCCESS',
+			response: normalize(response, schema.todo)
 		});
 	});
 
